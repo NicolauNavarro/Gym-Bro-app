8d9dc36a731f8481a486ad6aa4e5f663855ed553
Nicolau <149939145+NicolauNavarro@users.noreply.github.com>
2024-04-01 18:53:16 +0000

    Connect al frontend files

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const notion = require("./notion");
 const app = express();
 app.set("views", "./views");
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
